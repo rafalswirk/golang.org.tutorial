661ec4f30ae2e773e192a76b89df7b1861994c43
Rafał Świrk <rafalswirk@outlook.com>
2021-08-30 12:39:49 +0200

    - Adding simple view for page edit

--- WritingWebApplications/gowiki/wiki.go
+++ WritingWebApplications/gowiki/wiki.go
@@ -33,6 +33,21 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
 	if _, err := os.Stat("TestPage.txt"); err != nil {
 		p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
@@ -42,5 +57,6 @@ func main() {
 	}
 
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
